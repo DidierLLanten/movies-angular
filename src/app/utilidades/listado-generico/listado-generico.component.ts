bf90d152feffe6e962efd92743c29418950aba9c
DidierLLantenPc <didiervelez@hotmail.es>
2023-05-04 13:51:21 -0500

    30 proyeccion de contenido

---
+++ src/app/utilidades/listado-generico/listado-generico.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-generico',
+  templateUrl: './listado-generico.component.html',
+  styleUrls: ['./listado-generico.component.css'],
+})
+export class ListadoGenericoComponent implements OnInit {
+  @Input()
+  listado;
+  @Input()
+  listadoVacio;
+  constructor() {}
+  ngOnInit(): void {}
+}
