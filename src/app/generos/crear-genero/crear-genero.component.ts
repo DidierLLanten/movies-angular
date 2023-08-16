288edbf6b40d95318edbb5b3814298a1044bf038
DidierLLantenPc <didiervelez@hotmail.es>
2023-08-16 12:52:02 -0500

    S6-92 mostrando errores del WebApi

--- src/app/generos/crear-genero/crear-genero.component.ts
+++ src/app/generos/crear-genero/crear-genero.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { generoCreacionDTO } from '../genero';
 import { GenerosService } from '../generos.service';
+import { parsearErroresAPI } from 'src/app/utilidades/utilidades';
 @Component({
   selector: 'app-crear-genero',
   templateUrl: './crear-genero.component.html',
   styleUrls: ['./crear-genero.component.css'],
 })
 export class CrearGeneroComponent {
+  errores: string[] = [];
+
   constructor(private router: Router, private generosService: GenerosService) {}
 
   guardarCambios(genero: generoCreacionDTO) {
@@ -16,7 +19,10 @@ export class CrearGeneroComponent {
       () => {
         this.router.navigate(['/generos']);
       },
-      (error) => console.log(error.error.errors)
+      (error) => {
+        console.log('Errores al crear genero: ', error.error.errors);
+        this.errores = parsearErroresAPI(error);
+      }
     );
   }
 }
