4b4c59a5b29e59fd603e9e151d3758dd45c972b8
DidierLLantenPc <didiervelez@hotmail.es>
2023-08-18 10:33:32 -0500

    S6-100 Enviando imagenActor al WebAPI

--- src/app/actores/actores.service.ts
+++ src/app/actores/actores.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { actorCreacionDTO } from './actor';
 import { HttpClient } from '@angular/common/http';
+import { formatearFecha } from '../utilidades/utilidades';
 
 @Injectable({
   providedIn: 'root',
@@ -12,6 +13,21 @@ export class ActoresService {
   private apiURL = environment.apiURL + 'actores';
 
   public crear(actor: actorCreacionDTO) {
-    return this.http.post(this.apiURL, actor);
+    const formData = this.construirFormData(actor);
+    return this.http.post(this.apiURL, formData);
+  }
+
+  private construirFormData(actor: actorCreacionDTO): FormData {
+    const formData = new FormData();
+    formData.append('nombre', actor.nombre);
+    formData.append('biografia', actor.nombre);
+    if (actor.fechaNacimiento) {
+      formData.append('fechaNacimiento', formatearFecha(actor.fechaNacimiento));
+    }
+    if (actor.foto) {
+      formData.append('foto', actor.foto);
+    }
+
+    return formData;
   }
 }
