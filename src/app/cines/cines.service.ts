26ec6a20992b24eb83f80f40ee0d95a88e40e912
DidierLLantenPc <didiervelez@hotmail.es>
2023-08-20 17:11:09 -0500

    S6-107 Creando e implementando servicio listar cines con paginacion y eliminar

--- src/app/cines/cines.service.ts
+++ src/app/cines/cines.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { cineCreacionDTO } from './cine';
+import { cineCreacionDTO, cineDTO } from './cine';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +12,35 @@ export class CinesService {
 
   private apiURL = environment.apiURL + 'cines';
 
+  public obtenerTodos(
+    pagina: number,
+    cantidadRegistrosMostrar: number
+  ): Observable<any> {
+    let params = new HttpParams();
+    params = params.append('pagina', pagina.toString());
+    params = params.append(
+      'recordsPorPagina',
+      cantidadRegistrosMostrar.toString()
+    );
+    return this.http.get<cineDTO[]>(this.apiURL, {
+      observe: 'response',
+      params,
+    });
+  }
+
+  // public obtenerPorId(id: number): Observable<cineDTO> {
+  //   return this.http.get<cineDTO>(`${this.apiURL}/${id}`);
+  // }
+
   public crear(cine: cineCreacionDTO) {
     return this.http.post(this.apiURL, cine);
+  }
+
+  // public editar(id: number, cine: cineCreacionDTO) {
+  //   return this.http.put(`${this.apiURL}/${id}`, cine);
+  // }
+
+  public borrar(id: number) {
+    return this.http.delete(`${this.apiURL}/${id}`);
   }
 }
--- src/app/cines/indice-cines/indice-cines.component.ts
+++ src/app/cines/indice-cines/indice-cines.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CinesService } from '../cines.service';
+import { cineDTO } from '../cine';
+import { PageEvent } from '@angular/material/paginator';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-indice-cines',
   templateUrl: './indice-cines.component.html',
-  styleUrls: ['./indice-cines.component.css']
+  styleUrls: ['./indice-cines.component.css'],
 })
-export class IndiceCinesComponent {
+export class IndiceCinesComponent implements OnInit {
+  constructor(private cinesService: CinesService) {}
 
+  cines: cineDTO[];
+  columnasMostrar = ['id', 'nombre', 'acciones'];
+  cantidadTotalRegistros;
+  paginaActual = 1;
+  cantidadRegistrosMostrar = 10;
+
+  ngOnInit(): void {
+    this.cargarRegistros(this.paginaActual, this.cantidadRegistrosMostrar);
+  }
+
+  cargarRegistros(pagina: number, cantidadElementosMostrar: number) {
+    this.cinesService.obtenerTodos(pagina, cantidadElementosMostrar).subscribe(
+      (respuesta: HttpResponse<cineDTO[]>) => {
+        console.log('Respuesta: ', respuesta);
+        this.cines = respuesta.body;
+        this.cantidadTotalRegistros = respuesta.headers.get(
+          'cantidadTotalRegistros'
+        );
+      },
+      (error) => console.error('Error obtener cines: ', error)
+    );
+  }
+
+  actualizarPaginacion(datos: PageEvent) {
+    this.paginaActual = datos.pageIndex + 1;
+    this.cantidadRegistrosMostrar = datos.pageSize;
+    this.cargarRegistros(this.paginaActual, this.cantidadRegistrosMostrar);
+  }
+
+  borrar(id: number) {
+    this.cinesService.borrar(id).subscribe(
+      () => {
+        this.cargarRegistros(this.paginaActual, this.cantidadRegistrosMostrar);
+      },
+      (error) => console.error(error)
+    );
+  }
 }
