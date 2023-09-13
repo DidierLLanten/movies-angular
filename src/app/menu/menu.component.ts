24d04aa0173e93f3514c910458d2e1488618df5c
DidierLLantenPc <didiervelez@hotmail.es>
2023-09-13 12:36:25 -0500

    S7-129 created login component and logical to login and logout with JWT

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { SeguridadService } from '../seguridad/seguridad.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent {
-
+  constructor(public seguridadService: SeguridadService) {}
 }
--- src/app/seguridad/registro/registro.component.ts
+++ src/app/seguridad/registro/registro.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { credencialesUsuario } from '../seguridad';
 import { SeguridadService } from '../seguridad.service';
 import { parsearErroresAPI } from 'src/app/utilidades/utilidades';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
@@ -9,14 +10,15 @@ import { parsearErroresAPI } from 'src/app/utilidades/utilidades';
   styleUrls: ['./registro.component.css'],
 })
 export class RegistroComponent {
-  constructor(private seguridadService: SeguridadService) {}
+  constructor(private seguridadService: SeguridadService, private router: Router) {}
 
   errores: string[] = [];
 
   registrar(credenciales: credencialesUsuario) {
     this.seguridadService.registrar(credenciales).subscribe(
-      (respuesta) => {
-        console.log('Usuario registrado: ', respuesta);
+      (respuesta) => {        
+        this.seguridadService.guardarToken(respuesta);
+        this.router.navigate(['/']);
       },
       (errores) => {
         this.errores = parsearErroresAPI(errores);
